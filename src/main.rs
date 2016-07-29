d7463dba8fbc35eaf6be706ef87ed03c4c70f201
miro662 <miro662@gmail.com>
2016-07-29 17:55:41 +0200

    Added file support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
 mod bf;
 
 use bf::Brainfuck;
+use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
-    // Create new Brainfuck context
-    let mut brainfuck = Brainfuck::new();
+    // Get file to parse
+    let mut args = env::args();
+    if args.len() < 2 {
+        println!("usage: bf-rs file");
+    }
+    else {
+        // Create new Brainfuck context
+        let mut brainfuck = Brainfuck::new();
+        
+        // Load file to string
+        let mut program = String::new();
 
-    // Call simple code
-    brainfuck.call("+[,.]");
-    println!("");
+        let mut file = File::open(args.nth(1).unwrap())
+            .expect("Cannot load file!");
+
+        file.read_to_string(&mut program)
+            .expect("Cannot load program!");
+
+        println!("{}", program);
+        // Call simple code
+        brainfuck.call(&program);
+        println!("");
+    }
 }
